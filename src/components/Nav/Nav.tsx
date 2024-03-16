4cb935e78c64f1af72b2d55d5496d5d41e782480
Slo74design <salvologiudice@gmail.com>
2024-03-16 18:48:34 +0100

    flowbite
    installation, darkmode

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 import { Lang } from './Lang';
+import { DarkThemeToggle } from "flowbite-react";
 
 export const Nav = () => {
-  return (
-    <nav className="flex h-20 items-center justify-between px-12">
-      <Link href="/">YouTube Piloto</Link>
-
-      <Lang />
-    </nav>
-  );
+    return (
+        <nav className="flex h-20 items-center justify-between px-12 dark:bg-slate-500">
+            <Link href="/">YouTube Piloto</Link>
+            <Lang />
+            <DarkThemeToggle />
+        </nav>
+    );
 };
