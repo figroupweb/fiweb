7c117a403fdf9e91a82e855ad599b4dfab54e56b
figroupweb <website@fi-group.com>
2024-03-25 13:20:14 +0100

    formRT

--- src/app/[lang]/sector/page.tsx
+++ src/app/[lang]/sector/page.tsx
@@ -3,6 +3,7 @@ import Loading from "@/components/Loading";
 import { Locale } from "@/config/i18n.config";
 import { getAllPostsByLanguage } from "@/data";
 import { getDictionaryServerOnly } from "@/dictionaries/default-dictionary-server-only";
+import Script from "next/script";
 import React, { Suspense } from "react";
 
 interface Post {
@@ -23,6 +24,9 @@ export default async function SectorPage({
             <h3 className="text-lg font-semibold">
                 {dictionary.sector.name} new
             </h3>
+            <div data-form-id='2b67b781-b57f-ee11-8179-6045bd905432' data-form-api-url='https://public-eur.mkt.dynamics.com/api/v1.0/orgs/2fb5c271-8d0f-4705-9414-84152194938e/landingpageforms' data-cached-form-url='https://assets-eur.mkt.dynamics.com/2fb5c271-8d0f-4705-9414-84152194938e/digitalassets/forms/2b67b781-b57f-ee11-8179-6045bd905432' ></div>
+            <Script async defer id="form1" src="https://cxppusa1formui01cdnsa01-endpoint.azureedge.net/eur/FormLoader/FormLoader.bundle.js" />
+
             {news.map((post: Post) => (
                 <ContentPost key={post.uri} titlePost={post.title} />
             ))
